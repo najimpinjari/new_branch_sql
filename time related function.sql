4a269e9bbf870aa598778110ba0e75a99475d706
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-08 23:35:17 +0530

    Add files via upload

---
+++ time related function.sql
@@ -0,0 +1,11 @@
+use piramalemployee
+
+-- to get current time function 
+
+select getdate() --2023-12-08 22:45:11.977
+select CURRENT_TIMESTAMP--2023-12-08 22:46:10.787
+select SYSDATETIME()--2023-12-08 22:52:26.5752508
+select SYSDATETIMEOFFSET()--2023-12-08 22:52:26.5752508 +05:30
+select GETUTCDATE() --2023-12-08 17:22:26.573
+select SYSUTCDATETIME()--2023-12-08 17:22:26.5752508
+
